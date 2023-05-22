b96a860514c0eafd84beaf518755b7857676eb08
Kaarel-Richard Kaarelson <kaarel-richard.kaarelson@ut.ee>
2023-05-22 15:26:05 +0300

    Added a bear filter

--- chatbot.py
+++ chatbot.py
@@ -6,10 +6,10 @@ import re
 
 import estnltk
 
-filters = ["prill", "päikeseprill", "kübar", "kiiver", "nokamüts", "mask", "vunts", "deemon", "süda", "koer", "kloun", "pikachu"]
+filters = ["prill", "päikeseprill", "kübar", "kiiver", "nokamüts", "mask", "vunts", "deemon", "süda", "koer", "kloun", "pikachu", "karu"]
 head_filters = ["kübar", "kiiver", "nokamüts", "süda", "deemon"]
 eye_filters = ["prill", "päikeseprill"]
-face_filters = ["mask", "vunts", "koer", "kloun", "pikachu"]
+face_filters = ["mask", "vunts", "koer", "kloun", "pikachu", "karu"]
 
 
 def get_filters():
